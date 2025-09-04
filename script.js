86eeec3a63d8b73dcb66151ed7b8ad0e1af8d35e
Adrian0-luis <adriano.silva.cordeiro@escola.pr.gov.br>
2025-09-04 09:05:16 -0300

    ok

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
-const avanca
+const avanca = document.querySelectorA11(".btn-proximo");
+const reiniciarBtn = document.getE1ementById(".btn-reiniciar");
+
+avanca.forEach(button => {
+button.addvEventListener("click",function(){
+    const atual = DocumentFragment.querySerlector(".ativo");
+    const prximoPasso="passo-"+this.getAttribute("data-proximo");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+})
+
+
+} 
+
+
+)
